b1391bc5b20c60ef57afa92c2cff8d3a9fe9356e
Luuk <35917@ma-web.nl>
2023-05-16 13:11:17 +0200

    AudioClips

--- AloneInSpace/Assets/Scripts/FlashLIght_Scripts/FlashLight_Script.cs
+++ AloneInSpace/Assets/Scripts/FlashLIght_Scripts/FlashLight_Script.cs
@@ -8,12 +8,23 @@ public class FlashLight_Script : MonoBehaviour
 
     public GameObject orientation;
 
-    AudioSource flashLightOn_Sound;
+    AudioSource flashLightSound;
+    PlayerItems_Script myPlayerItemScript;
+
+    public AudioClip flaslightOn;
+    public AudioClip flaslightOff;
+
+    private void Awake()
+    {
+        myPlayerItemScript = FindObjectOfType<PlayerItems_Script>();
+    }
 
     private void Start()
     {
         FlashLight = GetComponent<Light>();
-        flashLightOn_Sound = GetComponent<AudioSource>();
+        flashLightSound = GetComponent<AudioSource>();
+
+        FlashLight.enabled = false;
     }
 
     private void Update()
@@ -24,14 +35,18 @@ public class FlashLight_Script : MonoBehaviour
 
     void SwitchLight()
     {
+        if (myPlayerItemScript.hasFlashlight == false)
+        {
+            return;
+        }
         if (Input.GetKeyDown(KeyCode.F) && FlashLight.enabled == true) 
         {
-            
+            flashLightSound.PlayOneShot(flaslightOff);
             FlashLight.enabled = false;
         }
         else if (Input.GetKeyDown(KeyCode.F) && FlashLight.enabled == false)
         {
-            flashLightOn_Sound.Play();
+            flashLightSound.PlayOneShot(flaslightOn);
             FlashLight.enabled = true;
         }
     }
